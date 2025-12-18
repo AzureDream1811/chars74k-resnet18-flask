57ea447e076e9acc791ccc6971c4cb0372aaf232
AzureDream <ducphamtan2098@gmail.com>
2025-12-18 11:40:17 +0700

    slight change

--- src/inference/Inference.py
+++ src/inference/Inference.py
@@ -1,13 +1,15 @@
 import torch
 from PIL import Image
+import torchvision
 
 from src.model.model_resnet18 import BuildResnet18
 from src.transform.image_transform import get_inference_transform
 
 CHARSET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
+
 class ResnetInference:
-    def __init__(self, model_path, device='cpu'):
+    def __init__(self, model_path, device="cpu"):
         self.device = device
         self.model = BuildResnet18(num_classes=62, pretrained=False)
         self.model.load_state_dict(torch.load(model_path, map_location=device))
@@ -15,16 +17,16 @@ class ResnetInference:
         self.model.eval()
 
         self.transform = get_inference_transform(image_size=64)
-        
+
     def predict(self, img_path):
         pil_img = Image.open(img_path).convert("RGB")
-        img: torch.Tensor = self.transform(pil_img)
-        img = img.unsqueeze(0).to(self.device)
-        
+        img_tensor = torchvision.transforms.ToTensor()(pil_img)
+        img = img_tensor.unsqueeze(0).to(self.device)
+
         with torch.no_grad():
             output = self.model(img)
 
         pred_idx = torch.argmax(output, dim=1).item()
-        pred_char = CHARSET[pred_idx]
+        pred_char = CHARSET[int(pred_idx)]
 
         return pred_char
